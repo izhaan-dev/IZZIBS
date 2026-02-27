4026956792497e7314e4f77fb68d44edd200adcf
izhaan-dev <izhaantat@gmail.com>
2026-02-27 08:17:26 +0530

    Mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import { FaPhoneAlt, FaEnvelope, FaWhatsapp } from 'react-icons/fa';
+import { FaPhoneAlt, FaEnvelope, FaWhatsapp, FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+
   // Dynamic logo path helper
   const logoPath = `${import.meta.env.BASE_URL}logo.png`;
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
 
   return (
     <nav className="navbar">
@@ -15,14 +27,18 @@ const Navbar = () => {
         </Link>
       </div>
       
-      <ul className="nav-links">
+      <div className="hamburger" onClick={toggleMobileMenu}>
+        {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+      </div>
+
+      <ul className={isMobileMenuOpen ? "nav-links mobile-active" : "nav-links"}>
         {/* NavLink automatically handles the "active" state */}
-        <li><NavLink to="/" end>Home</NavLink></li>
-        <li><NavLink to="/services">Services</NavLink></li>
-        <li><NavLink to="/domains">Domains</NavLink></li>
-        <li><NavLink to="/projects">Projects</NavLink></li>
-        <li><NavLink to="/about">About Us</NavLink></li>
-        <li><NavLink to="/contact">Contact Us</NavLink></li>
+        <li><NavLink to="/" end onClick={closeMobileMenu}>Home</NavLink></li>
+        <li><NavLink to="/services" onClick={closeMobileMenu}>Services</NavLink></li>
+        <li><NavLink to="/domains" onClick={closeMobileMenu}>Domains</NavLink></li>
+        <li><NavLink to="/projects" onClick={closeMobileMenu}>Projects</NavLink></li>
+        <li><NavLink to="/about" onClick={closeMobileMenu}>About Us</NavLink></li>
+        <li><NavLink to="/contact" onClick={closeMobileMenu}>Contact Us</NavLink></li>
       </ul>
 
       <div className="nav-icons">
